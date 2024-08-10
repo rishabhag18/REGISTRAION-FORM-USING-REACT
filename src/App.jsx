f5190b9ac2a651cde188471b16073155dd4c798c
rishabhag18 <18r.agarwal@gmail.com>
2024-08-11 01:15:39 +0530

    changes made- useState used for input values

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import styles from "./App.module.css";
+import { useState } from "react";
+const App = () => {
+  //const [userDetails, setUserDetails] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contact, setContact] = useState("");
+  const handleRegistration = (event) => {
+    event.preventDefault();
+    const userDetails={
+      firstName,
+      lastName,
+      email,
+      password,
+      contact,
+    };
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword(""); 
+    setContact("");
+    console.log(userDetails);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <form onSubmit={handleRegistration}>
+        <div className={styles.container}>
+          <div className={styles.innerContainer}>
+            <h1 className={styles.heading}>Sign Up</h1>
+            <p className={styles.description}>
+              Please fill in this form to create an account.
+            </p>
+            <label htmlFor="firstName">
+              <b>First Name </b>
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              placeholder="Enter first name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
 
-export default App
+            <label htmlFor="lastName">
+              <b>Last Name </b>
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="Enter last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+
+            <label htmlFor="email">
+              <b>Email </b>
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@xmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <label htmlFor="password">
+              <b>Password </b>
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+
+            <label htmlFor="contact">
+              <b>Phone Number </b>
+            </label>
+            <input
+              type="text"
+              name="contact"
+              id="contact"
+              placeholder="9876543211"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+              required
+            />
+
+            <button type="submit" className={styles.submitButton}>
+              Register
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+export default App;
